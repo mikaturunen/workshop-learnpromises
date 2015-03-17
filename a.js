22ae59691e5172866ca99a99085116300323d379
Mika Turunen <mika.turunen@ymail.com>
2015-03-17 18:28:02 +0200

    Fleshing out and learning how to build the actual lesson

--- a.js
+++ a.js
@@ -1,14 +1,1 @@
-var Hapi = require('hapi');
-var path = require('path');
-
-var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
-
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: {
-        file: path.join(__dirname, "exercises/promise__then/solution", '/index.html')
-    }
-});
-
-server.start();
+console.log("test");
