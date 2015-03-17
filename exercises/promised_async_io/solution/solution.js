ace523436ca3bf8d39560bf797f9e2e20afb3eaf
Mika Turunen <mika.turunen@ymail.com>
2015-03-17 19:31:46 +0200

    Exercise is now promisefied :)

--- exercises/promised_async_io/solution/solution.js
+++ exercises/promised_async_io/solution/solution.js
@@ -2,15 +2,6 @@ var Q = require("q");
 var fs = require("fs");
 var path = require("path");
 
-fs.readFile(
-    process.argv[2], 
-    { 
-        encoding: "utf8" 
-    }, 
-    function(error, data) {
-        if (error) {
-            throw error;
-        }
+Q.ninvoke(fs, "readFile", process.argv[2], { encoding: "utf8" })
+    .done(console.log, console.log);
 
-        console.log(data);
-    });
